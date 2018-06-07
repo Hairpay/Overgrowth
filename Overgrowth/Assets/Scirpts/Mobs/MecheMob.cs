de9b851d0376cbc4e3c6b814fc557a326e2ffb76
Hairpay <laurent.poliakoff@waterloo.eu>
2018-06-07 03:27:54 +0200

    first mob Done

---
+++ Overgrowth/Assets/Scirpts/Mobs/MecheMob.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Anima2D;
+
+public class MecheMob : MonoBehaviour
+{
+    public GameObject mobdossier;
+    public GameObject mobVisu;
+
+    public Color baseColor;
+    public Color actualColor;
+
+    public int life;
+    public int maxLife;
+
+    void Awake()
+    {
+        baseColor = mobVisu.GetComponent<SpriteMeshInstance>().color;
+        actualColor = baseColor;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        life = maxLife;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (life == 0)
+        {
+            gameObject.GetComponent<Explotron2D>().Explosion();
+            Destroy(mobdossier);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Orb")
+        {
+            life = life - 1;
+            actualColor.g = actualColor.g * 0.5f;
+            mobVisu.GetComponent<SpriteMeshInstance>().color = actualColor;
+
+        }
+    }
+
+}
---
+++ Overgrowth/Assets/Scirpts/Mobs/Mobanim.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mobanim : MonoBehaviour {
+
+    public Animator animob;
+    public Rigidbody2D mobody;
+
+    public Vector3 baseScale;
+    public GameObject mobroll;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animob = gameObject.GetComponent<Animator>();
+        baseScale = gameObject.transform.localScale;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.position = mobroll.transform.position;
+
+        if (mobody.velocity.x < -1)
+        {
+            gameObject.transform.localScale = new Vector3(baseScale.x, baseScale.y, baseScale.z);
+        }
+        else if (mobody.velocity.x > 1)
+        {
+            gameObject.transform.localScale = new Vector3(-baseScale.x, baseScale.y, baseScale.z);
+        }
+
+        if (Mathf.Abs(mobody.velocity.x) > 0.05f)
+        {
+            animob.SetBool("isWalking", true);
+        }
+        else
+        {
+            animob.SetBool("isWalking", false);
+        }
+    }
+}
