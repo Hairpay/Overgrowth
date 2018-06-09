34910ad735af6d6377eda52daa44c47d32407fed
sheido <af8081@gmail.com>
2018-06-09 20:37:43 +0200

    spawn player

---
+++ Overgrowth/Assets/Scirpts/SpawnPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour {
+
+    public GameObject PrefabPlayer;
+
+    private void Start ()
+    {
+        GameObject prefabPlayer;
+        prefabPlayer = Instantiate(PrefabPlayer, this.transform);
+        prefabPlayer.transform.localPosition = new Vector3(0,0,0);
+    }
+}
