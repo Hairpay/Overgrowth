3da7b2cb9a5c75f66715a1a4bec3086e8414dc80
Hairpay <laurent.poliakoff@waterloo.eu>
2018-08-21 17:36:50 +0200

    new tilemap in progress

---
+++ Overgrowth/Assets/Resources/LevelGestion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelGestion", menuName = "ScriptablesObjects/LevelGestion", order = 3)]
+public class LevelGestion : ScriptableObject
+{
+    public bool Event1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
