fef35f22a89d1bd170be4d4ee02d3dfcb488db94
Hairpay <laurent.poliakoff@waterloo.eu>
2018-09-10 17:44:00 +0200

    Boss Animation done (Idle, walk)

---
+++ Overgrowth/Assets/Scirpts/Mobs/Boss/ArmAttac.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmAttac : MonoBehaviour {
+
+    public GameObject character;
+    public Gestionnaire Gestionnaire;
+
+    public float t;
+
+    // Use this for initialization
+    void Start () {
+        character = GameObject.Find("character");
+        Gestionnaire = character.GetComponent<PowerUps>().Gestionnaire;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameObject.transform.position = new Vector3(
+              (Mathf.Lerp(gameObject.transform.position.x, character.transform.position.x, t)),
+              (Mathf.Lerp(gameObject.transform.position.x, character.transform.position.y, t)),
+              gameObject.transform.position.z);
+
+        if (t < 1)
+        {
+            t += 0.4f * Time.deltaTime;
+        }
+        else
+        {          
+           // t = 0;          
+        }
+    }
+}
