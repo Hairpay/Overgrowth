419f3fea58fbb7dae0bc08ec44ebc7e6a4dd3cfd
AYMERIC-PC\admin <xhin_12@hotmail.fr>
2018-09-12 16:29:29 +0200

    minimap prefab > Set the minimap in the canvas object > Set the variable Scale to a much lower value > Set the object 'img' to be centered on the character's relative position > Good luck ! Current issue: if moving against a wall, minimap is still translated

---
+++ Overgrowth/Assets/Scirpts/MapScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapScroller : MonoBehaviour
+{
+    public Transform m_Minimap;
+    [Range(0.0f, 1.0f)] public float m_MapScale = 0.3f;
+    public Transform m_Character;
+
+    private void LateUpdate()
+    {
+        Vector3 charVel = m_Character.GetComponent<Rigidbody2D>().velocity;
+        //if (charVel.x != 0)
+        //{
+        m_Minimap.Translate(-charVel.x * m_MapScale, -charVel.y * m_MapScale, 1);
+        //}
+        //if (charVel.y != 0)
+        //{
+
+        //}
+    }
+}
