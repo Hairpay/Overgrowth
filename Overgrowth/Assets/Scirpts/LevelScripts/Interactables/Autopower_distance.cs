36366a7101b3ade9f0bcf1e7b915e68cf5818f60
Hairpay <laurent.poliakoff@waterloo.eu>
2019-01-28 22:15:54 +0100

    Tube + some fixes (vine prefabs)

---
+++ Overgrowth/Assets/Scirpts/LevelScripts/Interactables/Autopower_distance.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Autopower_distance : MonoBehaviour
+{
+
+    public GameObject character;
+
+    public float activeDist;
+    public float dist;
+
+    public bool depower;
+
+    // Use this for initialization
+    void Start()
+    {
+        character = GameObject.Find("character");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(character.transform.position, transform.position);
+
+        if (Mathf.Abs(dist) < activeDist)
+        {
+            gameObject.GetComponent<PowerSource>().power = true;
+        }
+        else if (depower == true && Mathf.Abs(dist) > activeDist * 2)
+        {
+            gameObject.GetComponent<PowerSource>().power = false;
+        }
+    }
+}
